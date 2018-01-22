cb63fe87ed91addbae9125d252eea6f796c2e09f
Kamil Filipowski <filipowski1993@gmail.com>
2018-01-22 09:01:26 +0100

    Assets makeover (#1)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,31 +16,31 @@ class User < ActiveRecord::Base
   REFERRAL_STEPS = [
     {
       'count' => 5,
-      'html' => 'Shave<br>Cream',
+      'html' => '$200',
       'class' => 'two',
-      'image' =>  ActionController::Base.helpers.asset_path(
-        'refer/cream-tooltip@2x.png')
+      # 'image' =>  ActionController::Base.helpers.asset_path(
+      #   'refer/cream-tooltip@2x.png')
     },
     {
       'count' => 10,
-      'html' => 'Truman Handle<br>w/ Blade',
+      'html' => '$200',
       'class' => 'three',
-      'image' => ActionController::Base.helpers.asset_path(
-        'refer/truman@2x.png')
+      # 'image' => ActionController::Base.helpers.asset_path(
+      #   'refer/truman@2x.png')
     },
     {
       'count' => 25,
-      'html' => 'Winston<br>Shave Set',
+      'html' => '$200',
       'class' => 'four',
-      'image' => ActionController::Base.helpers.asset_path(
-        'refer/winston@2x.png')
+      # 'image' => ActionController::Base.helpers.asset_path(
+      #   'refer/winston@2x.png')
     },
     {
       'count' => 50,
-      'html' => 'One Year<br>Free Blades',
+      'html' => '$200',
       'class' => 'five',
-      'image' => ActionController::Base.helpers.asset_path(
-        'refer/blade-explain@2x.png')
+      # 'image' => ActionController::Base.helpers.asset_path(
+      #   'refer/blade-explain@2x.png')
     }
   ]
 
