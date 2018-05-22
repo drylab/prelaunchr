ee7e21806aec88edb0e58d09e237483877427ee2
Jakub Jatczak <j.jatczak@binarapps.com>
2018-05-22 16:45:56 +0200

    [fix] user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "Drylab <create.drylab.io>"
+  default from: "Drylab <noreply@drylab.io>"
 
   def signup_email(user)
     @user = user
     @twitter_message = "#Film-making is evolving. Excited for @drylab 3 to launch."
-    mail(to: user.email, from: 'noreply@create.drylab.io', subject: "Thanks for signing up!")
+    mail(to: user.email, subject: "Thanks for signing up!")
   end
 end
