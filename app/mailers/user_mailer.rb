dbeb9b2898b85ddabe393158a2307aa76654aeb4
Audun Vaaler <av@drylab.no>
2018-05-22 18:45:45 +0200

    Changes from Pontus

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def signup_email(user)
     @user = user
-    @twitter_message = "#Film-making is evolving. Excited for @drylab 3 to launch."
+    @twitter_message = "#Filmmaking is evolving. Excited for @drylab 3 to launch."
     mail(to: user.email, subject: "Thanks for signing up!")
   end
 end
